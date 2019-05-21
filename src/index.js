f5373fa39af84ff56849d26dfdecbc94919292f0
xujiangwei <xujiangwei@bytedance.com>
2019-05-21 15:01:44 +0800

    feat: first selector

--- src/index.js
+++ src/index.js
@@ -4,12 +4,13 @@ import './index.css';
 import App from './components/App';
 import store from './store';
 import { STORY_ARCHIVE } from './constants/actionTypes';
+import { getReadableStories } from './selectors/story';
 // import * as serviceWorker from './serviceWorker';
 
 
 ReactDOM.render(
 	<App 
-		stories={store.getState().storyState} 
+		stories={getReadableStories(store.getState())} 
 		onArchive={(id)=>{ store.dispatch({ type: STORY_ARCHIVE, id }) }} 
 	/>, 
 	document.getElementById('root')
---
+++ src/selectors/story.js
@@ -0,0 +1,10 @@
+const isNotArchived = archivedIds => story => 
+	archivedIds.indexOf(story.objectID) === -1;
+
+
+const getReadableStories = ({ storyState, archiveState }) => 
+	storyState.filter(isNotArchived(archiveState));
+
+export {
+	getReadableStories,
+};
