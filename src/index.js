612fe49e7f5301fed449678cd782835ca87a555a
xujiangwei <xujiangwei@bytedance.com>
2019-05-21 15:13:45 +0800

    feat: first action creator

--- src/index.js
+++ src/index.js
@@ -3,8 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
 import store from './store';
-import { STORY_ARCHIVE } from './constants/actionTypes';
 import { getReadableStories } from './selectors/story';
+import { doArchiveStory } from './actions/archive';
 // import * as serviceWorker from './serviceWorker';
 
 
@@ -13,7 +13,7 @@ function render () {
 	ReactDOM.render(
 		<App 
 			stories={getReadableStories(store.getState())} 
-			onArchive={(id)=>{ store.dispatch({ type: STORY_ARCHIVE, id }) }} 
+			onArchive={(id)=>{ store.dispatch(doArchiveStory(id)) }} 
 		/>, 
 		document.getElementById('root')
 	);
